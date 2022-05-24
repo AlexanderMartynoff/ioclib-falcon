885e5d95d4338797d86be273b9ce0d8deb9161a6
Александр <triplustri@mail.ru>
2022-05-24 14:18:40 +0400

    Change `ioclib-injector` version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 
 setup(
@@ -9,7 +9,7 @@ setup(
     version=__version__,
     packages=find_namespace_packages(include=['ioclib.*']),
     install_requires=[
-        'ioclib-injector>=1.0',
+        'ioclib-injector>=1.2',
         'falcon==3.0.1',
     ],
 )
