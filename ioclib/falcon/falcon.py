f69695de8b7433441cae6e9da6bf719a76b5624a
Александр <triplustri@mail.ru>
2022-05-28 01:47:36 +0400

    Refactoring

--- ioclib/falcon/falcon.py
+++ ioclib/falcon/falcon.py
@@ -1,24 +1,21 @@
-from typing import Any, Optional, Tuple, Dict, TypeVar
+from typing import Any, Optional, Tuple, Dict
 from falcon import Request
 from ioclib.injector import Requirement, void
 
 
-T = TypeVar('T')
-
-
 def header(default=void) -> Any:
-    return Requirement(None, None, 'falcon/header', default)
+    return Requirement[Any](None, None, 'falcon/header', default)
 
 
 def parameter(default=void) -> Any:
-    return Requirement(None, None, 'falcon/parameter', default)
+    return Requirement[Any](None, None, 'falcon/parameter', default)
 
 
 def context(default=void) -> Any:
-    return Requirement(None, None, 'falcon/context', default)
+    return Requirement[Any](None, None, 'falcon/context', default)
 
 
-def falcon_request_injector(requirement: Requirement[T], args: Tuple[Any, ...], kwargs: Dict[str, Any]):
+def falcon_request_injector(requirement: Requirement[Any], args: Tuple[Any, ...], kwargs: Dict[str, Any]):
     if not requirement.location.startswith('falcon'):
         raise LookupError()
 
@@ -28,19 +25,19 @@ def falcon_request_injector(requirement: Requirement[T], args: Tuple[Any, ...], 
         raise ValueError()
 
     if requirement.location == 'falcon/parameter':
-        if issubclass(requirement.tp, str):
+        if issubclass(requirement.type, str):
             return request.get_param(requirement.name, False)
 
-        if issubclass(requirement.tp, int):
+        if issubclass(requirement.type, int):
             return request.get_param_as_int(requirement.name, False)
 
-        if issubclass(requirement.tp, float):
+        if issubclass(requirement.type, float):
             return request.get_param_as_float(requirement.name, False)
 
-        if issubclass(requirement.tp, list):
+        if issubclass(requirement.type, list):
             return request.get_param_as_list(requirement.name, False)
 
-        if issubclass(requirement.tp, dict):
+        if issubclass(requirement.type, dict):
             return request.get_param_as_json(requirement.name, False)
 
         raise ValueError()
